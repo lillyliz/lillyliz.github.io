c58c7339602970caaf3a5b4a399d89ae9d53f60f
lillyliz <nicole.elizabeth.fb@gmail.com>
2026-05-18 17:15:46 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'julieta-v2';
+const CACHE = 'julieta-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
